886f8ae1b12e9b9a3a66ef09017050b289e3a100
Attila-Tenkes <39418380+Attila-Tenkes@users.noreply.github.com>
2023-05-22 09:30:39 +0200

    snowflake connection test

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,13 @@ streamlit.write('selected: ' , fruity_chioce)
 
 fruitVice_normalized = pandas.json_normalize(fruits.json())
 streamlit.dataframe(fruitVice_normalized)
+
+
+
+# connection test
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
